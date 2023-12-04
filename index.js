c01ba39df2b5a7d21901b70bb7007bf8a89b7e40
brysonkbarney <102181254+brysonkbarney@users.noreply.github.com>
2023-12-04 14:30:11 -0700

    db variables

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.RDS_HOSTNAME || "localhost",
-    user: process.env.RDS_USERNAME || "postgres",
-    password: process.env.RDS_PASSWORD || "admin",
-    database: process.env.RDS_DB_NAME || "activity_app",
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
     port: process.env.RDS_PORT || 5432,
     ssl: process.env.DB ? { rejectUnauthorized: false } : false,
   }, //a dictionary (keys and values)
