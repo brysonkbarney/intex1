fd27b25888a9bc43ac5cf1b58c146b962013fddb
CarolineTobl <119453126+CarolineTobl@users.noreply.github.com>
2023-12-04 10:13:06 -0700

    Index and Login Page

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ app.set('view engine', 'ejs'); //using ejs for our files.
 
 app.use(express.urlencoded({extended:true}));
 
+// Get request for the landing page
+app.get('/', (req, res) => {
+    res.render('index'); // This will render the index.ejs file
+  });
+
+//Get request for the login page
+app.get('/login', (req, res) => {
+    res.render('login'); // This will render the login.ejs file
+  });
+
+//Get Request for creating an account in our login page.
+//add account user name and password to our account table 
+
+  
+
 app.listen(port, () => console.log('Server is Listening')); //last line!!
