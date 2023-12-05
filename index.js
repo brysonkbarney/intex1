40b4052aee1306418ba26510c32ac183b36bef0b
brysonkbarney <102181254+brysonkbarney@users.noreply.github.com>
2023-12-05 13:15:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME || "localhost",
+    user: process.env.RDS_USERNAME || "carolinetobler",
+    password: process.env.RDS_PASSWORD || "P0ftim122-",
+    database: process.env.RDS_DB_NAME || "ebdb",
     port: process.env.RDS_PORT || 5432,
     ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
   }, //a dictionary (keys and values)
@@ -45,25 +45,26 @@ app.post("/storeLogin", (req, res) => {
 });
 
 app.post("/findLogin", (req, res) => {
-    knex.select("userName", "password")
-        .from("login")
-        .where({
-            userName: req.query.userName,
-            password: req.query.password  // Add this line to include password in the query
-        })
-        .then(login => {
-            if (login.length > 0) {
-                // If the login array is not empty, it means a match was found
-                res.redirect("/");
-            } else {
-                // No matching credentials found
-                res.status(401).send("Invalid credentials");
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ err });
-        });    
+  knex
+    .select("userName", "password")
+    .from("login")
+    .where({
+      userName: req.query.userName,
+      password: req.query.password, // Add this line to include password in the query
+    })
+    .then((login) => {
+      if (login.length > 0) {
+        // If the login array is not empty, it means a match was found
+        res.redirect("/");
+      } else {
+        // No matching credentials found
+        res.status(401).send("Invalid credentials");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err });
+    });
 });
 
 //Get request for the login page
@@ -85,87 +86,91 @@ app.get("/login", (req, res) => {
 
 // Get request for the add data page
 app.get("/addData", (req, res) => {
-    res.render("addData"); // This will render the addData.ejs file
-  });
-  
-//Get request for the add data survey page
-app.post("/storeData", async (req, res) => {
-    const { 
-        age, 
-        gender, 
-        relationshipStatus, 
-        occupationStatus, 
-        organizations, 
-        platforms, 
-        socialMediaUsage, 
-        avgDailyTime, 
-        purpose, 
-        distracted, 
-        restless, 
-        easilyDistracted, 
-        worried, 
-        concentration, 
-        comparison, 
-        comparisonFeelings, 
-        validation, 
-        depression, 
-        interests, 
-        sleep, 
-        location
-    } = req.body;
-    
-    try {
-        await knex.transaction(async trx => {
-            const [userId] = await trx('user').insert({
-                age,
-                gender,
-                relationshipStatus,
-                occupationStatus,
-                socialMediaUsage, 
-                avgDailyTime, 
-                purpose, 
-                distracted, 
-                restless, 
-                easilyDistracted, 
-                worried, 
-                concentration, 
-                comparison, 
-                comparisonFeelings, 
-                validation, 
-                depression, 
-                interests, 
-                sleep, 
-                location
-            }).returning('userID');
-
-            if (organizations) {
-                const userOrganizations = Array.isArray(organizations) ? organizations : [organizations];
-                const organizationInserts = userOrganizations.map(org => ({
-                    userID: userId,
-                    organizationName: org
-                }));
-                await trx('user_organization').insert(organizationInserts);
-            }
-
-            if (platforms) {
-                const userPlatforms = Array.isArray(platforms) ? platforms : [platforms];
-                const platformInserts = userPlatforms.map(platform => ({
-                    userID: userId,
-                    platformName: platform
-                }));
-                await trx('user_platform').insert(platformInserts);
-            }
-
-            await trx.commit();
-            res.send("Record added successfully. Thank you for your response.");
-        });
-    } catch (error) {
-        console.error("Error details:", error);
-        res.status(500).send("Failed to store data");
-    }
+  res.render("addData"); // This will render the addData.ejs file
 });
 
+//Get request for the add data survey page
+app.post("/storeData", async (req, res) => {
+  const {
+    age,
+    gender,
+    relationshipStatus,
+    occupationStatus,
+    organizations,
+    platforms,
+    socialMediaUsage,
+    avgDailyTime,
+    purpose,
+    distracted,
+    restless,
+    easilyDistracted,
+    worried,
+    concentration,
+    comparison,
+    comparisonFeelings,
+    validation,
+    depression,
+    interests,
+    sleep,
+    location,
+  } = req.body;
 
+  try {
+    await knex.transaction(async (trx) => {
+      const [userId] = await trx("user")
+        .insert({
+          age,
+          gender,
+          relationshipStatus,
+          occupationStatus,
+          socialMediaUsage,
+          avgDailyTime,
+          purpose,
+          distracted,
+          restless,
+          easilyDistracted,
+          worried,
+          concentration,
+          comparison,
+          comparisonFeelings,
+          validation,
+          depression,
+          interests,
+          sleep,
+          location,
+        })
+        .returning("userID");
+
+      if (organizations) {
+        const userOrganizations = Array.isArray(organizations)
+          ? organizations
+          : [organizations];
+        const organizationInserts = userOrganizations.map((org) => ({
+          userID: userId,
+          organizationName: org,
+        }));
+        await trx("user_organization").insert(organizationInserts);
+      }
+
+      if (platforms) {
+        const userPlatforms = Array.isArray(platforms)
+          ? platforms
+          : [platforms];
+        const platformInserts = userPlatforms.map((platform) => ({
+          userID: userId,
+          platformName: platform,
+        }));
+        await trx("user_platform").insert(platformInserts);
+      }
+
+      await trx.commit();
+      res.send("Record added successfully. Thank you for your response.");
+    });
+  } catch (error) {
+    console.error("Error details:", error);
+    res.status(500).send("Failed to store data");
+  }
+});
 
 //Get request for the dashboard
 app.get("/dashboard", (req, res) => {
@@ -173,13 +178,39 @@ app.get("/dashboard", (req, res) => {
 });
 
 //Get request for the view all data page
-app.get('/viewData', (req, res) =>{
-  knex.select('userID', 'timestamp', 'age', 'gender', 'relationshipStatus', 'occupationStatus', 'socialMediaUsage', 'avgDailyTime', 'purpose', 'distracted', 'restless', 'easilyDistracted', 'worried', 'concentration', 'comparison', 'comparisonFeelings', 'validation', 'depression', 'interests', 'sleep', 'location').from('user').then(user => {
-      res.render('viewData', {mydata: user});
-  }).catch(err => {
+app.get("/viewData", (req, res) => {
+  knex
+    .select(
+      "userID",
+      "timestamp",
+      "age",
+      "gender",
+      "relationshipStatus",
+      "occupationStatus",
+      "socialMediaUsage",
+      "avgDailyTime",
+      "purpose",
+      "distracted",
+      "restless",
+      "easilyDistracted",
+      "worried",
+      "concentration",
+      "comparison",
+      "comparisonFeelings",
+      "validation",
+      "depression",
+      "interests",
+      "sleep",
+      "location"
+    )
+    .from("user")
+    .then((user) => {
+      res.render("viewData", { mydata: user });
+    })
+    .catch((err) => {
       console.log(err);
-      res.status(500).json({err});
-  })
-})
+      res.status(500).json({ err });
+    });
+});
 
 app.listen(port, () => console.log("Server is Listening")); //last line!!
