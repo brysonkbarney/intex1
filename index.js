af9c30bc790453039687006b0a0dd9281b41c17c
CarolineTobl <119453126+CarolineTobl@users.noreply.github.com>
2023-12-06 13:09:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ const knex = require("knex")({
   client: "pg",
   connection: {
     host: process.env.RDS_HOSTNAME || "localhost",
-    user: process.env.RDS_USERNAME || "postgres",
-    password: process.env.RDS_PASSWORD || "admin",
+    user: process.env.RDS_USERNAME || "carolinetobler",
+    password: process.env.RDS_PASSWORD || "P0ftim1225-",
     database: process.env.RDS_DB_NAME || "ebdb",
     port: process.env.RDS_PORT || 5432,
     ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
