cd98ec7146c737edbfdd778a677b7bf772890b47
brysonkbarney <102181254+brysonkbarney@users.noreply.github.com>
2023-12-07 09:28:19 -0700

    comments

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-const express = require("express"); //call the express module into app
+// Importing necessary modules
+const express = require("express"); // Call the express module into the app
+let app = express(); // Server to listen, handle routes, and have middleware
+let path = require("path"); // Module to make it easier to call files
+const port = process.env.PORT || 3000; // Port where the server is listening
 
-let app = express(); //server website, listen and have routes
+// Setting up EJS as the view engine
+app.set("view engine", "ejs");
 
-let path = require("path"); //makes it easier to call files
-
-const port = process.env.PORT || 3000; //Where the server is listening on.
-
-app.set("view engine", "ejs"); //using ejs for our files.
-
+// Middleware setup for parsing URL-encoded and JSON bodies
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static("public"));
-
-const session = require("express-session");
 app.use(express.json());
 
-// Configure express-session
+// Static files middleware setup
+app.use(express.static("public"));
+
+// Setting up express-session middleware
+const session = require("express-session");
 app.use(
   session({
-    secret: "provomediaimpact", // You should use a long, random string in production
+    secret: "provomediaimpact", // Use a long, random string in production
     resave: false,
     saveUninitialized: true,
     //cookie: { secure: false }, // Set to true if using https
   })
 );
 
+// Setting up the connection to PostgreSQL database using Knex
 const knex = require("knex")({
   client: "pg",
   connection: {
+    // Connection details
     host: process.env.RDS_HOSTNAME || "localhost",
     user: process.env.RDS_USERNAME || "carolinetobler",
     password: process.env.RDS_PASSWORD || "P0ftim1225-",
     database: process.env.RDS_DB_NAME || "ebdb",
     port: process.env.RDS_PORT || 5432,
     ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
-  }, //a dictionary (keys and values)
-}); //parameters go in the ({})
-
-// Get request for the landing page
-app.get("/", (req, res) => {
-  res.render("index"); // This will render the index.ejs file
+  },
 });
 
-//adding users to the data table
-app.post("/storeLogin", (req, res) => {
-  const { username, password } = req.body; // Make sure these names match your form input names
+// Handling GET request for the landing page
+app.get("/", (req, res) => {
+  res.render("index"); // Render the index.ejs file
+});
 
+// Handling POST request for adding users to the data table
+app.post("/storeLogin", (req, res) => {
+  // Extracting data from the request body
+  const { username, password } = req.body;
+
+  // Inserting user data into the login table
   knex("login")
     .insert({
-      username: username, // This should match the column name in your database
+      username: username,
       password: password,
     })
     .then(() => {
@@ -56,15 +60,16 @@ app.post("/storeLogin", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
+      // Handling the case where the username already exists
       res.send(
         '<script>alert("Username already exists. Please choose another one to continue!"); window.location.href = "/create"; </script>'
       );
     });
 });
 
-//searching the table for matches
+// Handling POST request for searching the table for matches
 app.post("/findLogin", async (req, res) => {
-  // Use 'username' and 'password' to match the form input names
+  // Extracting data from the request body
   const { username, password } = req.body;
 
   console.log("Received body:", req.body);
@@ -72,23 +77,26 @@ app.post("/findLogin", async (req, res) => {
   console.log("Extracted password:", password);
 
   try {
-    // Ensure the column name matches the database column name
+    // Searching for a user in the login table
     const user = await knex
       .select("*")
       .from("login")
       .where({
-        username: username, // Column name in database: 'userName'
+        username: username,
         password: password,
       })
       .first();
 
     if (user) {
+      // Setting session variables for a logged-in user
       req.session.userInfo = user;
       req.session.loggedIn = true;
+      // Redirecting with a success message
       res.send(
         '<script>alert("Your login credentials were validated!"); window.location.href = "/"; </script>'
       );
     } else {
+      // Redirecting with an error message for invalid credentials
       res.send(
         '<script>alert("Login Credentials Invalid!"); window.location.href = "/login"; </script>'
       );
@@ -99,43 +107,50 @@ app.post("/findLogin", async (req, res) => {
   }
 });
 
-//Get request for the login page
+// Handling GET request for the login page
 app.get("/login", (req, res) => {
-  const isLoggedIn = req.session.loggedIn || false; // Check if user is logged in
-  res.render("login", { isLoggedIn: isLoggedIn }); // Pass the logged-in status to the EJS template
+  // Checking if the user is logged in
+  const isLoggedIn = req.session.loggedIn || false;
+  // Rendering the login page and passing the logged-in status to the EJS template
+  res.render("login", { isLoggedIn: isLoggedIn });
 });
 
-// Get request for the add data page
+// Handling GET request for the add data page
 app.get("/addData", (req, res) => {
-  res.render("addData"); // This will render the addData.ejs file
+  res.render("addData"); // Render the addData.ejs file
 });
 
+// Handling GET request for the create account page
 app.get("/create", (req, res) => {
-  res.render("create"); // This will render the addData.ejs file
+  res.render("create"); // Render the create.ejs file
 });
 
+// Handling GET request for editing login information
 app.get("/editLogin", (req, res) => {
   console.log("------Testing------");
 
   // Ensure req.session.userInfo is defined
   if (!req.session.userInfo) {
-    // Redirect or handle the case where userinfo is not defined
-    res.redirect("/login"); // You may want to redirect to the login page or handle it in a different way
+    // Redirect to the login page if userinfo is not defined
+    res.redirect("/login");
     return;
   }
 
   console.log(req.session.userInfo.username);
   console.log(req.session.userInfo.password);
-  res.render("editLogin", { userinfo: req.session.userInfo }); // Pass userinfo to the template
+  // Render the editLogin page and pass userinfo to the template
+  res.render("editLogin", { userinfo: req.session.userInfo });
 });
 
+// Handling POST request for editing login information
 app.post("/editLogin", (req, res) => {
   const { username, password } = req.body;
 
+  // Updating login information in the login table
   knex("login")
     .where("username", req.session.userInfo.username)
     .update({
-      username: username, // Updated with the new username from the form
+      username: username,
       password: password,
     })
     .then(() => {
@@ -152,6 +167,7 @@ app.post("/editLogin", (req, res) => {
     });
 });
 
+// Mappings for organization and platform types
 const organizationMapping = {
   University: 1,
   Government: 2,
@@ -173,8 +189,10 @@ const platformMapping = {
   TikTok: 9,
   None: 10,
 };
-//Get request for the add data survey page
+
+// Handling POST request for storing survey data
 app.post("/storeData", async (req, res) => {
+  // Extracting survey data from the request body
   const {
     age,
     gender,
@@ -200,12 +218,14 @@ app.post("/storeData", async (req, res) => {
   } = req.body;
 
   try {
+    // Using a transaction to ensure atomicity
     await knex.transaction(async (trx) => {
       const currentTimestamp = new Date();
-      const location = "Provo"; // Set location to 'Provo'
+      const location = "Provo"; // Setting location to 'Provo'
+      // Inserting data into the user table
       const [userInsertResult] = await trx("user")
         .insert({
-          timestamp: currentTimestamp, // Include this line
+          timestamp: currentTimestamp,
           age,
           gender,
           relationshipStatus,
@@ -230,7 +250,7 @@ app.post("/storeData", async (req, res) => {
 
       const userId = userInsertResult.userID;
 
-      // Inserting into user_organization
+      // Inserting data into the user_organization table
       if (organizations) {
         const userOrganizations = Array.isArray(organizations)
           ? organizations
@@ -242,7 +262,7 @@ app.post("/storeData", async (req, res) => {
         await trx("user_organization").insert(organizationInserts);
       }
 
-      // Inserting into user_platform
+      // Inserting data into the user_platform table
       if (platforms) {
         const userPlatforms = Array.isArray(platforms)
           ? platforms
@@ -255,8 +275,9 @@ app.post("/storeData", async (req, res) => {
       }
 
       await trx.commit();
+      // Redirecting with a success message
       res.send(
-        '<script>alert("Thank you for your response! Your record was added sucsessfully."); window.location.href = "/"; </script>'
+        '<script>alert("Thank you for your response! Your record was added successfully."); window.location.href = "/"; </script>'
       );
     });
   } catch (error) {
@@ -265,17 +286,16 @@ app.post("/storeData", async (req, res) => {
   }
 });
 
-//Get request for the dashboard
+// Handling GET request for the dashboard
 app.get("/dashboard", (req, res) => {
-  res.render("dashboard"); // This will render the login.ejs file
+  res.render("dashboard"); // Render the dashboard.ejs file
 });
 
-//Get request for the view all data page
-// Get request for the view all data page
+// Handling GET request for viewing all data
 app.get("/viewData", (req, res) => {
   // Check if the user is logged in
   if (!req.session.loggedIn) {
-    // User is not logged in, redirect to the login page
+    // Redirect to the login page if the user is not logged in
     res.send(
       '<script>alert("Please login to view records."); window.location.href = "/login"; </script>'
     );
@@ -355,7 +375,7 @@ app.get("/viewData", (req, res) => {
     });
 });
 
-//get request for a single record
+// Handling GET request for viewing a single record
 app.get("/searchData/:userNum", (req, res) => {
   knex
     .select(
@@ -424,4 +444,6 @@ app.get("/searchData/:userNum", (req, res) => {
       res.status(500).json({ err });
     });
 });
-app.listen(port, () => console.log("Server is Listening")); //last line!!
+
+// Start the server
+app.listen(port, () => console.log("Server is Listening"));
